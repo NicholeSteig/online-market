b45b4595faf671949c19252e2bb4710c5e9101a0
NicholeSteig <nichole.steig@gmail.com>
2020-02-16 11:29:59 -0800

    basic structure

---
+++ client/src/pages/cart.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import components
+
+const Cart = () => {
+    return(
+
+    );
+};
+
+export default Cart;
---
+++ client/src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import components
+
+const Home = () => {
+    return(
+
+    );
+};
+
+esport default Home;
